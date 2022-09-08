e7863061d01339544ea86f73c40d0282440d02a3
denisZlobich <sgsdgk9232@gmail.com>
2022-09-08 13:52:43 +0300

    2. Add models

---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,26 @@
+export enum TopLevelCategory {
+    Courses,
+    Services,
+    Books,
+    Products,
+}
+
+export class TopPageModel {
+    fistCategory: TopLevelCategory;
+    secondCategory: string;
+    title: string;
+    category: string;
+    advantages: {
+        title: string;
+        description: string;
+    }[];
+    seoText: string;
+    tags: string[];
+    tagsTitle: string;
+    hh?: {
+        count: number;
+        juniorSalary: number;
+        middleSalary: number;
+        seniorSalary: number;
+    };
+}
