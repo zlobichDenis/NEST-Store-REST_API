96bdf776247cde3f38e1952a772ac5854e2ab01b
denisZlobich <sgsdgk9232@gmail.com>
2022-09-13 18:19:18 +0300

    9. Implement jwt auth strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { User, AuthSchema } from './schemas/auth.schema';
 import { AuthService } from './auth.service';
 import { getJWTConfig } from '../../config/jwt.config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
@@ -24,8 +26,10 @@ import { getJWTConfig } from '../../config/jwt.config';
           inject: [ConfigService],
           useFactory: getJWTConfig,
       }),
+      ConfigModule,
+      PassportModule,
   ],
 
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -9,12 +9,15 @@ import {
 	HttpStatus,
 	UsePipes,
 	ValidationPipe,
+	UseGuards,
 } from '@nestjs/common';
 
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { errorMessages } from './constants';
 
+@UseGuards(JwtAuthGuard)
 @Controller('review')
 export class ReviewController {
 	constructor(
@@ -36,6 +39,7 @@ export class ReviewController {
 		}
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('byProduct/:productId')
 	async getByProduct(@Param('productId') productId: string) {
 		return this.reviewService.findByProductId(productId);
